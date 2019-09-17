f71b514775044b480f7dcba688349a8697abaae4
aurelieclavier <aurelie.cissokho@gmail.com>
2019-09-17 16:53:21 +0200

    add method from DicoLoad.java

--- src/fr/wcs/afa/dicoutils/DicoLoad.java
+++ src/fr/wcs/afa/dicoutils/DicoLoad.java
@@ -1,5 +1,34 @@
 package fr.wcs.afa.dicoutils;
 
-public class DicoLoad {
+import java.io.BufferedReader;
+import java.io.FileReader;
 
+public class DicoLoad {
+	
+	public String[] loadFile(String path) {
+		
+		try {
+			// initialisation des variables;
+			int i = 0;
+			//On défini le nombre d'index dans la tableau file;
+			String[] file = new String[336532];
+			//Lecture du fichier dictionnaire.txt;
+			BufferedReader readFile = new BufferedReader(new FileReader(path));
+			//On initialise line;
+			String line;
+			//On boucle sur ReadFile en utilisant line
+			while((line = readFile.readLine()) != null) {
+				//On stock file[i] dans line pour que readLine() 
+				//ne renvoie pas null à la fin du tableau ([336532 + 1]) 
+				file[i++] = line;
+			}
+			//On close le fichier
+			readFile.close();
+			return file;
+		}catch (Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
 }
